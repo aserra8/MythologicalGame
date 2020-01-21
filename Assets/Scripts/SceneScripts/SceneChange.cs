16250abafcdb4089df5936ff756bd063af5712ad
aserra <aserra8@boscdelacoma.cat>
2020-01-21 19:44:50 +0100

    Fist Commit

---
+++ Assets/Scripts/SceneScripts/SceneChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public string changeScene;
+    public Vector2 playerPosition;
+    public VectorValue playerStorage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            playerStorage.initialValue = playerPosition;
+            SceneManager.LoadScene(changeScene);
+        }
+    }
+}
