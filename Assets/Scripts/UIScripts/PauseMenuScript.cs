4f8a2ee5c5be6bca613099ce49e2716793ad54f3
aserra <aserra8@boscdelacoma.cat>
2020-02-18 19:30:23 +0100

    Commit

---
+++ Assets/Scripts/UIScripts/PauseMenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    private bool isPaused = false;
+    public string mainMenuScene;
+    public GameObject pausePanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+        pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            ChangePause();            
+        }
+    }
+
+    public void ChangePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void QuitToMain()
+    {
+        //Descomentar quan tinguem el menú principal creat
+        //SceneManager.LoadScene(mainMenuScene);
+        //Time.timeScale = 1f;
+    }
+}
